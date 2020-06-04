71ad7b77c8adf1bfb4a04e9858910effcdf0b12e
silvareal <sylvernusakubo@yahoo.com>
2020-06-04 23:07:33 +0100

    enums

---
+++ projects/ngx-loading-x/src/lib/utils/enums.ts
@@ -0,0 +1,31 @@
+// Spinner Configuration
+export const SPINNER_CONFIG = {
+    'three-strings': {
+        divs: 3,
+        class: 'sk-three-strings'
+    },
+    'wandering-cubes': {
+        divs: 2,
+        class: 'sk-wandering-cubes'
+    }
+};
+
+// SPinner Types
+export enum SPINNER {
+    threeStrings = 'three-strings',
+    wanderingCubes = 'wandering-cubes'
+}
+
+// Element Positions
+export enum POSITION {
+    bottomCenter = 'bottom-center',
+    bottomLeft = 'bottom-left',
+    bottomRight = 'bottom-right',
+    centerCenter = 'center-center',
+    centerLeft = 'center-left',
+    centerRight = 'center-right',
+    topCenter = 'top-center',
+    topLeft = 'top-left',
+    topRight = 'top-right'
+}
+
