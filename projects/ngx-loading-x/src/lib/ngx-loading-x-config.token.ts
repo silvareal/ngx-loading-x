46ec9ae98a18963a932c25060815c892d93dac4b
silvareal <sylvernusakubo@yahoo.com>
2020-06-04 23:08:29 +0100

    injectortoken for standalone providers

--- projects/ngx-loading-x/src/lib/ngx-loading-x-config.token.ts
+++ projects/ngx-loading-x/src/lib/ngx-loading-x-config.token.ts
@@ -1,3 +1,4 @@
 import { InjectionToken } from "@angular/core";
+import { NgxLoadingXConfig } from './utils/NgxLoadingXConfig.interface';
 
-export const NGX_LOADING_X_TOKEN = new InjectionToken<>('ngxLoadingXConfig')
+export const NGX_LOADING_X_TOKEN = new InjectionToken<NgxLoadingXConfig>('ngxLoadingXConfig')
