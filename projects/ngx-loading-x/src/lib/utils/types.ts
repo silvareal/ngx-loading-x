e1e657e647a246707c092b455aad224f61e1f8be
silvareal <sylvernusakubo@yahoo.com>
2020-06-04 18:57:24 +0100

    spinnerType and positionType added

---
+++ projects/ngx-loading-x/src/lib/utils/types.ts
@@ -0,0 +1,14 @@
+export type SpinnerType =
+    | 'three-strings'
+    | 'wandering-cubes';
+
+export type PositionType =
+    | 'bottom-center'
+    | 'bottom-left'
+    | 'bottom-right'
+    | 'center-center'
+    | 'center-left'
+    | 'center-right'
+    | 'top-center'
+    | 'top-left'
+    | 'top-right';
