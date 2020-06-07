95974218610b8722d5b17117ab7739bb820addca
silvareal <sylvernusakubo@yahoo.com>
2020-06-07 21:14:50 +0100

    blur if show=true else set blur as none

--- src/app/ngx-loading-xblur.directive.ts
+++ src/app/ngx-loading-xblur.directive.ts
@@ -1,23 +1,29 @@
-import { Directive, Renderer2, ElementRef, Input, OnInit } from '@angular/core';
+import { Directive, Renderer2, ElementRef, Input, OnInit, OnChanges } from '@angular/core';
 import { DEFAULT_CONFIG } from 'projects/ngx-loading-x/src/lib/utils/NgxLoadingXConfig.constants';
 
 @Directive({
   selector: '[NgxLoadingXBlur]'
 })
 
-export class NgxLoadingXBlurDirective implements OnInit {
+export class NgxLoadingXBlurDirective implements OnChanges {
   @Input() blur;
+  @Input() show;
 
   constructor(private renderer: Renderer2, private elementRef: ElementRef) {
     this.blur = DEFAULT_CONFIG.bgBlur;
-   }
+    this.show = DEFAULT_CONFIG.show;
+  }
 
-  ngOnInit(): void {
-    if (DEFAULT_CONFIG.show === true) {}
-    console.log(DEFAULT_CONFIG.show['loading'])
+  ngOnChanges() {
+    if (this.show) {
       const blurValue = `blur(${this.blur}px)`;
       this.renderer.setStyle(this.elementRef.nativeElement, '-webkit-filter', blurValue);
       this.renderer.setStyle(this.elementRef.nativeElement, 'filter', blurValue);
-    
+    }
+    else {
+      const blurValue = `none`;
+      this.renderer.setStyle(this.elementRef.nativeElement, '-webkit-filter', blurValue);
+      this.renderer.setStyle(this.elementRef.nativeElement, 'filter', blurValue);
+    }
   }
 }
