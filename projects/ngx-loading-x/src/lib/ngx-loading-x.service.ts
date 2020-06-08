832129acc2dcd9b1d8e9b56db2527f5e419b355a
silvareal <sylvernusakubo@yahoo.com>
2020-06-08 21:56:06 +0100

    inject provider config

--- projects/ngx-loading-x/src/lib/ngx-loading-x.service.ts
+++ projects/ngx-loading-x/src/lib/ngx-loading-x.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { DEFAULT_CONFIG } from 'projects/ngx-loading-x/src/lib/utils/NgxLoadingXConfig.constants';
+import { Injectable, Optional, Inject } from '@angular/core';
+import { NGX_LOADING_X_TOKEN } from './ngx-loading-x-config.token';
+import { NgxLoadingXConfig } from './utils/NgxLoadingXConfig.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NgxLoadingXService {
+  defaultConfig: NgxLoadingXConfig;
 
-  constructor() { }
+  constructor(@Optional() @Inject(NGX_LOADING_X_TOKEN) private config: NgxLoadingXConfig) { 
+    this.defaultConfig = {...DEFAULT_CONFIG}
+    if (this.config) {
+      this.defaultConfig = {...DEFAULT_CONFIG, ...this.config}
+    }
+  }
+
+  getDefaultConfig(): NgxLoadingXConfig {
+    return {...this.defaultConfig}
+  }
 }
