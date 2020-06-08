4a964042cee3b70fe14ae4116e1f6b262366066d
silvareal <sylvernusakubo@yahoo.com>
2020-06-08 21:47:16 +0100

    update token

--- projects/ngx-loading-x/src/lib/ngx-loading-x-config.token.ts
+++ projects/ngx-loading-x/src/lib/ngx-loading-x-config.token.ts
@@ -1,4 +1,4 @@
 import { InjectionToken } from "@angular/core";
 import { NgxLoadingXConfig } from './utils/NgxLoadingXConfig.interface';
 
-export const NGX_LOADING_X_TOKEN = new InjectionToken<NgxLoadingXConfig>('ngxLoadingXConfig')
+export const NGX_LOADING_X_TOKEN = new InjectionToken<NgxLoadingXConfig>('ngxLoadingXConfig.config')
