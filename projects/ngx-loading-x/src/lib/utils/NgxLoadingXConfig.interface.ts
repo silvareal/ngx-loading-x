af543a2870c74bc118d209dc200df11779cd8d61
silvareal <sylvernusakubo@yahoo.com>
2020-06-09 22:40:01 +0100

    optional interface types

--- projects/ngx-loading-x/src/lib/utils/NgxLoadingXConfig.interface.ts
+++ projects/ngx-loading-x/src/lib/utils/NgxLoadingXConfig.interface.ts
@@ -1,15 +1,15 @@
 import { PositionType, SpinnerType } from './types';
 
 export interface NgxLoadingXConfig {
-    show: boolean;
-    bgBlur: number;
-    bgColor: string;
-    bgOpacity: number;
-    bgLogoUrl: string;
-    bgLogoUrlPosition: PositionType;
-    bgLogoUrlSize: number;
-    spinnerType: SpinnerType;
-    spinnerSize: number;
-    spinnerColor: string;
-    spinnerPosition: PositionType
+    show?: boolean;
+    bgBlur?: number;
+    bgColor?: string;
+    bgOpacity?: number;
+    bgLogoUrl?: string;
+    bgLogoUrlPosition?: PositionType;
+    bgLogoUrlSize?: number;
+    spinnerType?: SpinnerType;
+    spinnerSize?: number;
+    spinnerColor?: string;
+    spinnerPosition?: PositionType
 }
