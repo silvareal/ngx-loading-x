51cff8ca86abfed0481c271be7d75e463ed424b8
TaylorHo <taylor@hotay.dev>
2023-08-15 18:07:21 -0300

    fix: supporting angular v16

--- projects/ngx-loading-x/src/lib/ngx-loading-x.component.spec.ts
+++ projects/ngx-loading-x/src/lib/ngx-loading-x.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NgxLoadingXComponent } from './ngx-loading-x.component';
 
@@ -6,7 +6,7 @@ describe('NgxLoadingXComponent', () => {
   let component: NgxLoadingXComponent;
   let fixture: ComponentFixture<NgxLoadingXComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NgxLoadingXComponent ]
     })
